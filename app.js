5e41d6a60587ebf73af2e64e69b8d7d98b5a2093
banfstory <denis_chi_yiu@hotmail.com>
2021-12-28 17:08:58 +1100

    init

---
+++ app.js
@@ -0,0 +1,106 @@
+$(function() {
+	let header = $('#header');
+	let hamburger = $('.hamburger');
+	let nav = $('.nav-links');
+
+	// determine if changes to the header is required or not by returning true or false
+	function triggerScroll() {
+		let position = window.scrollY;
+ 		if(position > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// set header class to scrolled which allows for changes to logo name color and background color of navigation
+	function setClassScrolled(state) {
+		if(state) {
+			header.addClass('scrolled'); 
+		} else {
+			header.removeClass('scrolled');
+		}
+	}
+
+	// change color for the navigation links or hamburger button
+	function setHeaderColor(state) {
+		if(state) {
+			hamburger.addClass('change');
+			nav.addClass('change');
+		} else {
+			hamburger.removeClass('change');
+			nav.removeClass('change');
+		}
+	}
+
+	// this will open up the hamburger menu
+	hamburger.click(() => {
+		let status = hamburger.hasClass('active');
+		if(status) { // hide menu
+			let state = triggerScroll();
+			setClassScrolled(state);
+			setHeaderColor(state);
+			hamburger.removeClass('active');
+			nav.removeClass('active');
+		} else { // show menu
+			setHeaderColor(true);
+			hamburger.addClass('active');
+			nav.addClass('active');
+		}
+	});
+
+	// will change color for the header when scrolled however if hamburger menu do not change certain elements of the header
+	document.addEventListener('scroll', () => {
+		let state = triggerScroll();
+		setClassScrolled(state);
+		let status = hamburger.hasClass('active');
+		if(!status || $(window).width() > 991) {
+			setHeaderColor(state);
+		}
+	});
+
+	// default changes when window size is above 991 or page is refreshed
+	function defaultChange() {
+		let state = triggerScroll();
+		setClassScrolled(state);
+		setHeaderColor(state);
+	}
+
+	// work around for scroll event listener just incase hamburger menu is still active
+	$(window).resize(function() {
+		if($(window).width() > 991) {
+			defaultChange();
+		}
+	});
+	defaultChange();
+});
+
+let skillShowHide = document.getElementsByClassName('show-hide-all')[0];
+let showHideBtn = skillShowHide.getElementsByTagName('button')[0];
+let circleGrow = skillShowHide.getElementsByClassName('circle-grow')[0];
+let skills = document.getElementById('skills');
+let skillItems = skills.getElementsByClassName('skill-items');
+let skillsHeader = skills.getElementsByTagName('h2')[0];
+
+showHideBtn.onmouseover = () => {
+	circleGrow.classList.add('scale-up');
+	skillsHeader.classList.add('change-heading-color');
+}
+
+showHideBtn.onmouseleave = () => {
+	circleGrow.classList.remove('scale-up');
+	skillsHeader.classList.remove('change-heading-color');
+}
+
+showHideBtn.onclick = (e) => {
+	if(skills.classList.contains("show-all")) {
+		skills.classList.remove("show-all");
+		showHideBtn.innerHTML = "SHOW MORE";
+		skills.scrollIntoView(true);
+	} else {
+		skills.classList.add("show-all");
+		showHideBtn.innerHTML = "SHOW LESS";
+		skills.scrollIntoView(true);
+	}
+}
+
